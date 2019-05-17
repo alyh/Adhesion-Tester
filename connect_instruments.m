b65406f68a31938befb1b19ddef16ec078012f1c
sitruklaq <44737287+sitruklaq@users.noreply.github.com>
2019-05-17 12:09:09 -0400

    0

--- connect_instruments.m
+++ connect_instruments.m
@@ -4,14 +4,21 @@ function [force_gauge, step_motor] = connect_instruments()
     % These serial ports should be changed depending on the computer
 
     % Connect Force Gauge
-    force_gauge = serial('/dev/tty.SLAB_USBtoUART');
+    force_gauge = serial('COM6');
     force_gauge.Baudrate = 115200;
     fopen(force_gauge);
 
     % Connect Step Motor
-    step_motor = serial('/dev/tty.usbserial-FTTAGLYU');
+    step_motor = serial('COM7');
     fopen(step_motor);
     step_motor.RecordDetail ='Verbose';
     record(step_motor)
+
+ fprintf(step_motor, 'TA 0.1'); % acceleration time (s)
+ fprintf(step_motor, 'TD 0.1'); % de-accerleration time (s)
+ fprintf(step_motor, 'VS 1000'); % starting velocity (56 = 10um/s)
+ fprintf(step_motor, 'VR 1000'); % running velocity
+ fprintf(step_motor, 'DIS 5000'); % distance to travel
+ fprintf(step_motor, 'MI'); % start motion
 end
 
